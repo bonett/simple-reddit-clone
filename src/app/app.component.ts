94e35139548abf85785cb18a53c083d75ed35999
Wilfrido Bonett <wbonett10@gmail.com>
2020-05-22 18:38:59 -0500

    Add sort article list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,10 +18,14 @@ export class AppComponent {
     ];
   }
 
-  _onAddArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
-    console.log(`Adding article title: ${title.value} and link: ${link.value}`); this.articles.push(new Article(title.value, link.value, 0));
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    this.articles.push(new Article(title.value, link.value, 0));
     title.value = '';
     link.value = '';
     return false;
+  }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
   }
 }
